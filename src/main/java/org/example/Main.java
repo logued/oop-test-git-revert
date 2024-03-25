b0c560f5b83a06fd77cac98d8da65c554d8efe59
dermo <dermot.logue@dkit.ie>
2024-03-25 13:41:57 +0100

    stage2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,15 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        fn1();
+        fn2();
+    }
+    public static void fn1(){
+        System.out.println("fn1 was called.");
+    };
+    public  static void fn2() {
+        System.out.println("fn2 was called.");
     }
 }
