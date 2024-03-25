67853da4d4424192bc6a192d542b327a0a8265ab
dermo <dermot.logue@dkit.ie>
2024-03-25 13:55:47 +0100

    branch 3 - 3 fns()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,15 @@ public class Main {
         System.out.println("Hello world!");
         fn1();
         fn2();
+        fn3();
     }
     public static void fn1(){
         System.out.println("fn1 was called.");
     };
     public  static void fn2() {
         System.out.println("fn2 was called.");
+    }
+    public  static void fn3() {
+        System.out.println("fn3 was called.");
     }
 }
